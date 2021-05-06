6ed3f88482e576a80cd0a2e8c2919966338e365f
phbomfim <paulobommfim@gmail.com>
2021-05-06 18:34:44 -0300

    Add date validation

--- app/models/movimentacao.rb
+++ app/models/movimentacao.rb
@@ -3,6 +3,7 @@ class Movimentacao < ApplicationRecord
   belongs_to :local_armazenamento
 
   validates :tipo, length: {maximum: 2}, presence: true
-  validates :data, presence: true
+  validates :data, presence: true, 
+            inclusion: {in: (Date.new(2021,01,01)..Date.new(2021,01,31))}
            
 end
