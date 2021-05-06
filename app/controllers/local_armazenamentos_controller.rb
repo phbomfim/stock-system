6bf9b6cf5a42b4fff2152a421b4ed05b8c7ae019
phbomfim <paulobommfim@gmail.com>
2021-05-06 11:22:31 -0300

    Fix produto and local controllers/views

--- app/controllers/local_armazenamentos_controller.rb
+++ app/controllers/local_armazenamentos_controller.rb
@@ -4,4 +4,29 @@ class LocalArmazenamentosController < ApplicationController
         @locais = LocalArmazenamento.all
     end
 
+    def new
+        @local = LocalArmazenamento.new
+    end
+
+    def create
+        contador = 0
+        CSV.foreach("movimentacao_de_estoque.csv", headers: true , header_converters: :symbol) do |row| 
+            nome_deposito, data, tipo_de_movimentacao, nome_produto, quantidade = row
+             if tipo_de_movimentacao[1] == " E"
+                ## Verifica se o produto existe, caso não, cria
+
+                @local = LocalArmazenamento.find_or_create_by(nome: nome_deposito[1])
+
+                if @local.save
+                    contador += 1
+                else  
+                    redirect_to local_armanezamentos_url and return
+                end
+            end
+        end
+
+        puts contador
+        redirect_to home_index_path
+
+    end
 end
