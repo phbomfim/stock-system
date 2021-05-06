fed01130a2e7ed7309b0f68a356434f1f428ee4b
phbomfim <paulobommfim@gmail.com>
2021-05-06 16:15:29 -0300

    Update movimentacaos controller with errors log

--- app/controllers/movimentacaos_controller.rb
+++ app/controllers/movimentacaos_controller.rb
@@ -17,10 +17,11 @@ class MovimentacaosController < ApplicationController
             @produto = Produto.find_or_create_by(nome: nome_produto[1])
             @local = LocalArmazenamento.find_or_create_by(nome: nome_deposito[1])
             
-            ## Primeiro caso = ENTRADA
-            if tipo_de_movimentacao[1] == " E"
+            if @produto.valid? && @local.valid?
 
-                if @produto.present? && @local.present?
+                ## Primeiro caso = ENTRADA
+                if tipo_de_movimentacao[1] == " E"
+
                     @estoque = Estoque.find_by(produto_id: @produto.id, local_armazenamento_id: @local.id)
                     # Se o registro desse deposito/produto já existir, incrementa quantidade
                     if @estoque.present?
@@ -31,40 +32,68 @@ class MovimentacaosController < ApplicationController
                         @estoque = Estoque.new(produto_id: @produto.id, local_armazenamento_id: @local.id, quantidade: quantidade[1].to_f)
                         if @estoque.save
                             contador += 1
+                        else
+                            if @estoque.valid? == false
+                                File.write("log.txt","Estoque erro: #{@estoque.errors.messages}", mode: "a")
+                                File.write("log.txt", "\n", mode: "a")
+                            end
                         end
                     end
-                end
+                
 
-                ## Registra a movimentação de ENTRADA
-                @movimentacao = Movimentacao.new(data: data[1].to_date,tipo: tipo_de_movimentacao[1], quantidade: quantidade[1].to_f,produto_id: Produto.find(@produto.id).id,local_armazenamento_id: LocalArmazenamento.find(@local.id).id)
+                    ## Registra a movimentação de ENTRADA
+                    @movimentacao = Movimentacao.new(data: data[1].to_date,tipo: tipo_de_movimentacao[1], quantidade: quantidade[1].to_f,produto_id: Produto.find(@produto.id).id,local_armazenamento_id: LocalArmazenamento.find(@local.id).id)
 
-                if @movimentacao.save
-                    contador += 1
-                end
+                    if @movimentacao.save
+                        contador += 1
+                    else
+                        if @movimentacao.valid? == false
+                            puts "#{@movimentacao.errors.messages}"
+                            File.write("log.txt","Movimentacao erro: #{@movimentacao.errors.messages}", mode: "a")
+                            File.write("log.txt", "\n", mode: "a")
+                        end
+                    end
 
-            ## Segundo caso = SAIDA
-            elsif tipo_de_movimentacao[1] == " S"
+                ## Segundo caso = SAIDA
+                elsif tipo_de_movimentacao[1] == " S"
 
-                if @produto.present? && @local.present?
                     @estoque = Estoque.find_by(produto_id: @produto.id, local_armazenamento_id: @local.id)
                     # Se o registro desse deposito/produto já existir
                     if @estoque.present?
                         if @estoque.quantidade >= quantidade[1].to_f    #Verifica quantidade disponivel
                             @estoque.update_attribute(:quantidade, @estoque.quantidade -= quantidade[1].to_f)
                             contador += 1
+                        else
+                            File.write("log.txt", "Quantidade não suficiente - possui #{@estoque.quantidade} - precisa #{quantidade[1].to_f} \n", mode: "a")
+                            File.write("log.txt", "\n", mode: "a")
+                        end
+                    else
+                        File.write("log.txt","Estoque não encontrado!", mode: "a")
+                        File.write("log.txt", "\n", mode: "a")
+                    end
+
+                    ## Registra a movimentação de SAIDA
+                    @movimentacao = Movimentacao.new(data: data[1].to_date,tipo: tipo_de_movimentacao[1], quantidade: quantidade[1].to_f,produto_id: Produto.find(@produto.id).id,local_armazenamento_id: LocalArmazenamento.find(@local.id).id)
+
+                    if @movimentacao.save
+                        contador += 1
+                    else
+                        if @movimentacao.valid? == false
+                            puts "#{@movimentacao.errors.messages}"
+                            File.write("log.txt","Movimentacao erro: #{@movimentacao.errors.messages}", mode: "a")
+                            File.write("log.txt", "\n", mode: "a")
                         end
                     end
                 end
-
-                ## Registra a movimentação de SAIDA
-                @movimentacao = Movimentacao.new(data: data[1].to_date,tipo: tipo_de_movimentacao[1], quantidade: quantidade[1].to_f,produto_id: Produto.find(@produto.id).id,local_armazenamento_id: LocalArmazenamento.find(@local.id).id)
-
-                if @movimentacao.save
-                    contador += 1
-                end
-            else
-                redirect_to home_index_path  and return
+            elsif @produto.valid? == false 
+                File.write("log.txt","Falha na movimentação - Produto erro : #{@produto.errors.messages}", mode: "a")
+                File.write("log.txt", "\n", mode: "a")
+            else  
+                File.write("log.txt","Falha na movimentação - Depósito erro: #{@local.errors.messages}", mode: "a")
+                File.write("log.txt", "\n", mode: "a")
             end
         end
+
+        redirect_to home_index_path 
     end
 end
