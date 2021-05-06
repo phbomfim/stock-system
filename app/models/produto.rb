1bb56cc4bb795e5f6d5be68b9738b8b4bc4e87f5
phbomfim <paulobommfim@gmail.com>
2021-05-06 09:34:26 -0300

    Change produto model

--- app/models/produto.rb
+++ app/models/produto.rb
@@ -1,2 +1,8 @@
 class Produto < ApplicationRecord
+    validates :nome, uniqueness: true
+
+    def self.assign_from_row(row)
+        produto = Produto.new(name: produto_nome[1]).first_or_initalize
+        user.assign_attributes row.to_hash.slice(:nome)
+    end
 end
