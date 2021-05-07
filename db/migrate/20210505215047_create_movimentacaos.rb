6ebe706914efe1dae69bf47f9f4c5eb52f5f04e8
phbomfim <paulobommfim@gmail.com>
2021-05-07 00:05:02 -0300

    Fix migration label name error

--- db/migrate/20210505215047_create_movimentacaos.rb
+++ db/migrate/20210505215047_create_movimentacaos.rb
@@ -2,7 +2,7 @@ class CreateMovimentacaos < ActiveRecord::Migration[6.1]
   def change
     create_table :movimentacaos do |t|
       t.references :produto, null: false, foreign_key: true
-      t.references :localarmazenamento, null: false, foreign_key: true
+      t.references :local_armazenamento, null: false, foreign_key: true
       t.string :tipo
       t.integer :quantidade
 
