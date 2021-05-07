64eb74760dcc531dff5f2c9653af52b7374022a1
phbomfim <paulobommfim@gmail.com>
2021-05-06 22:38:02 -0300

    Add comments in controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,19 +1,16 @@
 class HomeController < ApplicationController
-    ## Produto.find(ID) - Encontrar pela ID 
-    ## @produto = Produto.find_or_create_by(nome: nome_produto) - Encontrar por termo
-    ## Atualizar por termo se o produto existir
-    ## @produto.update_attribute(:quantidade, @produto.quantidade +=1 ) if @produto.exist?
 
     def index
         @produtos = Produto.all
         @movimentacoes = Movimentacao.all
         @locais = LocalArmazenamento.all
-        @estoques = Estoque.all
+        @estoques = Estoque.all.order("local_armazenamento_id")
     end
 
     def create
     end
 
+    ## Realizar a importanção do arquivo para o sistema
     def import
         uploaded_io = params[:file]
         File.open(Rails.root.join(uploaded_io.original_filename), 'wb') do |file|
