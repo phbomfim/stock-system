aeb82982e140a920ef090c7e1640a9dd1389e95f
phbomfim <paulobommfim@gmail.com>
2021-05-06 22:38:15 -0300

    Define error date message

--- app/models/movimentacao.rb
+++ app/models/movimentacao.rb
@@ -4,6 +4,6 @@ class Movimentacao < ApplicationRecord
 
   validates :tipo, length: {maximum: 2}, presence: true
   validates :data, presence: true, 
-            inclusion: {in: (Date.new(2021,01,01)..Date.new(2021,01,31))}
+            inclusion: {in: (Date.new(2021,01,01)..Date.new(2021,01,31)), message: "Fora do periodo"}
            
 end
